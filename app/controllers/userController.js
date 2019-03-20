d548157a22ab02154556d26a0db611cb503efb96
Gustavo Martins do Santos <gustavoms97@hotmail.com>
2019-03-20 13:24:46 -0300

    Whole CRUD for Users developed.

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,17 +3,72 @@ const { User } = require('../models/');
 
 const router = express.Router();
 
-router.get('/users', (req, res) => {}); // Listar todos
-router.post('/users', (req, res) => {}); // Criar
-router.get('/users/:id', (req, res) => {}); // Buscar
-router.put('/users/:id', (req, res) => {}); // Editar
-router.delete('/users/:id', (req, res) => {}); // Deletar
+// Listar todos
+router.get('/', async (req, res) => {
+  const userList = await User.findAll({});
+  userList.forEach((user) => {
+    console.log(user);
+    delete user.dataValues.password;
+  });
 
-// Registrar
-router.post('/register', async (req, res) => {
-  console.log(req.body);
+  return res.status(200).send(userList);
+});
+
+// Criar
+router.post('/', async (req, res) => {
   const user = await User.create(req.body);
-  res.status(201).send(user);
+  delete user.password;
+  return res.status(201).send(user);
+});
+
+// Buscar
+router.get('/:id', async (req, res) => {
+  if (!req.params.id) {
+    return res.status(400).send({ message: 'Id was not provided in the URL' });
+  }
+
+  const user = await User.findAll({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  delete user.password;
+
+  return res.status(200).send(user);
+});
+
+// Editar
+router.put('/:id', async (req, res) => {
+  if (!req.params.id) {
+    return res.status(400).send({ message: 'Id was not provided in the URL' });
+  }
+
+  const updateValues = {};
+  if (req.body.email) {
+    updateValues.email = req.body.email;
+  }
+  if (req.body.name) {
+    updateValues.name = req.body.name;
+  }
+  const response = await User.update(updateValues, { where: { id: req.params.id } });
+  const message = response[0] === 1 ? 'User was updates successfully' : 'User does not exists';
+  return res.status(200).send({ message });
+});
+
+// Deletar
+router.delete('/:id', async (req, res) => {
+  if (!req.params.id) {
+    return res.status(400).send({ message: 'Id was not provided in the URL' });
+  }
+
+  const response = await User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  const message = response > 0 ? 'Successfully deleted!' : 'Data not found';
+  return res.status(200).send({ message });
 });
 
 module.exports = app => app.use('/user', router);
